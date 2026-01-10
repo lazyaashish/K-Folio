5edadcaedd91e768ad9b0e69eb88dd91bc6d9a82
garimakumarix29 <garimakumari.x29@gmail.com>
2026-01-10 22:49:56 +0530

    Post component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,19 @@
 
 import Background from "./pages/Landing/Background"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import LandingPage from "./pages/Landing/LandingPage";
+import Home from "./pages/Home"
 import "./App.css";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: 12 }}>
-        <Link to="/" style={{ marginRight: 12 }}>
-          Home
-        </Link>
-        <Link to="/signin">Sign In</Link>
-      </nav>
-
       <Routes>
         {/* Landing Page */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/signin" element={<Background />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import LandingBackground from "@/components/Landing Page/LandingBackground";
+import PostSection from "@/components/Post/PostSection";
+
+const Home = () => {
+  return (
+    <LandingBackground activeSection="home">
+      <div className="w-full py-10">
+      <PostSection />
+      </div>
+    </LandingBackground>
+  );
+};
+
+export default Home;
